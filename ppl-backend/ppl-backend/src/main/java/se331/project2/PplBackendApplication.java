afe0ffd907732f57d635250f3051586489de1c36
PlermNichakorn <153720088+PlermNichakorn@users.noreply.github.com>
2024-10-18 21:44:48 +0700

    Add files via upload

---
+++ ppl-backend/ppl-backend/src/main/java/se331/project2/PplBackendApplication.java
@@ -0,0 +1,13 @@
+package se331.project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PplBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PplBackendApplication.class, args);
+    }
+
+}
---
+++ ppl-backend/ppl-backend/src/test/java/se331/project2/PplBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se331.project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PplBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
