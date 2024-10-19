0083c4ad3e058237a921e79a2465247a405c83d7
Nonlanee <nonneenie@gmail.com>
2024-10-19 20:41:33 +0700

    create init app

---
+++ ppl-backend/ppl-backend/src/main/java/se331/project2/rest/config/initApp.java
@@ -0,0 +1,73 @@
+package se331.project2.rest.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import se331.project2.rest.entity.Country;
+import se331.project2.rest.repository.CountryRepository;
+
+@Component
+@RequiredArgsConstructor
+public class initApp implements ApplicationListener<ApplicationReadyEvent> {
+    final CountryRepository countryRepository;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent){
+        countryRepository.save(Country.builder()
+                .id(110L)
+                .countryName("United States")
+                .description("The United States first participated in the Olympics in the 1896 Summer Olympics and has sent athletes to every Summer Olympics since. Athletes have won a total of 2,826 medals: 1,125 gold, 907 silver, and 794 bronze.")
+                .image("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/330px-Flag_of_the_United_States.svg.png")
+                .gold(40L)
+                .silver(44L)
+                .bronze(42L)
+                .rank(1L)
+                .sport("Basketball")
+                .build());
+        countryRepository.save(Country.builder()
+                .id(112L)
+                .countryName("United States")
+                .description("The United States first participated in the Olympics in the 1896 Summer Olympics and has sent athletes to every Summer Olympics since. Athletes have won a total of 2,826 medals: 1,125 gold, 907 silver, and 794 bronze.")
+                .image("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/330px-Flag_of_the_United_States.svg.png")
+                .gold(40L)
+                .silver(44L)
+                .bronze(42L)
+                .rank(1L)
+                .sport("Basketball")
+                .build());
+        countryRepository.save(Country.builder()
+                .id(113L)
+                .countryName("United States")
+                .description("The United States first participated in the Olympics in the 1896 Summer Olympics and has sent athletes to every Summer Olympics since. Athletes have won a total of 2,826 medals: 1,125 gold, 907 silver, and 794 bronze.")
+                .image("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/330px-Flag_of_the_United_States.svg.png")
+                .gold(40L)
+                .silver(44L)
+                .bronze(42L)
+                .rank(1L)
+                .sport("Basketball")
+                .build());
+        countryRepository.save(Country.builder()
+                .id(114L)
+                .countryName("United States")
+                .description("The United States first participated in the Olympics in the 1896 Summer Olympics and has sent athletes to every Summer Olympics since. Athletes have won a total of 2,826 medals: 1,125 gold, 907 silver, and 794 bronze.")
+                .image("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/330px-Flag_of_the_United_States.svg.png")
+                .gold(40L)
+                .silver(44L)
+                .bronze(42L)
+                .rank(1L)
+                .sport("Basketball")
+                .build());
+        countryRepository.save(Country.builder()
+                .id(115L)
+                .countryName("United States")
+                .description("The United States first participated in the Olympics in the 1896 Summer Olympics and has sent athletes to every Summer Olympics since. Athletes have won a total of 2,826 medals: 1,125 gold, 907 silver, and 794 bronze.")
+                .image("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/330px-Flag_of_the_United_States.svg.png")
+                .gold(40L)
+                .silver(44L)
+                .bronze(42L)
+                .rank(1L)
+                .sport("Basketball")
+                .build());
+    }
+}
